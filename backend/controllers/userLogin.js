6c0f41266902e4157d7e9a1fab4b09e832c67556
THUMMALAPALLI NIKHIL CHOWDARY <tnikhilchowdary@gmail.com>
2025-11-24 13:19:17 -0600

    Login Crud Operation done

--- backend/controllers/userLogin.js
+++ backend/controllers/userLogin.js
@@ -1,13 +1,35 @@
 import User from "../schema/userSchema.js";
+import bcrypt from "bcrypt";
 
 export const Login = async (req, res) => {
-    try{
-        const {name, email, password} = req.body;
-        const loginData = await User.findOne({});
-        if(!login)
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({
+                message: "User Not Found"
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({
+                message: "Incorrect Password"
+            });
+        }
+
+        return res.status(200).json({
+            message: "Login Successfully",
+            user
+        });
+
+    } catch (error) {
+        console.error("Error in Logging in", error);
+        return res.status(500).json({
+            message: "Error in Login",
+            error
+        });
     }
-    catch(error){
-        console.error("Error in Logging in");
-        return res.status()
-    }
-}
+};
--- backend/routers/expenseRouter.js
+++ backend/routers/expenseRouter.js
@@ -1,10 +1,13 @@
 import {getExpense, addExpenses, updateExpenses, deleteExpenses} from "../controllers/expenseControllers.js";
 import {register} from "../controllers/userControllers.js"
+import {Login} from "../controllers/userLogin.js";
+
+
 import express from "express";
 
 const router = express.Router();
-
-router.post("/user", register)
+router.post("/login", Login);
+router.post("/register", register)
 router.get("/", getExpense);
 router.post("/", addExpenses);
 router.put("/:id", updateExpenses)
