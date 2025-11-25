7d698749730b39dea302f24ce9087bf36858f73f
THUMMALAPALLI NIKHIL CHOWDARY <tnikhilchowdary@gmail.com>
2025-11-25 12:20:13 -0600

    Home Page Setup

--- expense/src/App.jsx
+++ expense/src/App.jsx
@@ -1,7 +1,11 @@
 import Expense from "../src/components/ExpenseForm.jsx";
 import Login from "../src/components/Login.jsx";
 import Sign from "../src/components/Signup.jsx";
+import Home from "../src/components/Home.jsx";
+
+
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
 
 function App() {
 
@@ -11,6 +15,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Sign />} />
         <Route path="/expense" element={<Expense />}/>
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   )
