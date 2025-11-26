0e701d6238fbbe42f82704c4b0f873f00cec92f1
THUMMALAPALLI NIKHIL CHOWDARY <tnikhilchowdary@gmail.com>
2025-11-26 12:30:25 -0600

    Mongoose Schema Updated

--- backend/routers/expenseRouter.js
+++ backend/routers/expenseRouter.js
@@ -1,16 +1,17 @@
 import {getExpense, addExpenses, updateExpenses, deleteExpenses} from "../controllers/expenseControllers.js";
 import {register} from "../controllers/userControllers.js"
 import {Login} from "../controllers/userLogin.js";
+import {auth} from "..//middleware/auth.js";
 
 
 import express from "express";
 
 const router = express.Router();
-router.post("/login", Login);
+router.post("/login",Login);
 router.post("/register", register)
-router.get("/", getExpense);
-router.post("/", addExpenses);
-router.put("/:id", updateExpenses)
-router.delete("/:id", deleteExpenses);
+router.get("/", auth, getExpense);
+router.post("/", auth, addExpenses);
+router.put("/:id", auth, updateExpenses)
+router.delete("/:id", auth, deleteExpenses);
 
 export default router;
--- backend/schema/ExpenseSchema.js
+++ backend/schema/ExpenseSchema.js
@@ -18,6 +18,11 @@ const ExpenseSchema = new mongoose.Schema({
     date:{
         type:Date,
         required:true
+    },
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
     }
 }, {timestamps:true});
 
